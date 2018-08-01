13d3f0eaccd86d99fdf17921d560e73abbc0eb89
smlong203 <s.m.long203@outlook.com>
2018-08-01 10:30:06 -0500

    updated

---
+++ general JS code snippits/IF_ELSE_Templates.js
@@ -0,0 +1,20 @@
+if (appleSauceContion){
+
+      }
+
+      if (condition){
+        // block of code when condition is true
+      } else{
+        // block of code when condition is false
+      }
+
+      if (condition1 && condition2){
+        // block of code when condition1 and condition2 is true
+      }else if (condition1){
+        // block of code when ONLY condition1 is true
+      } else if (condition2){
+        // block of code when ONLY condition2 is true
+      }
+      else{
+        // block of code when both condtions are false
+      }
